48936b6e0398e7f6e40983a46e30ddd6bb83e81d
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2023-11-16 23:02:56 -0300

    Update vetorMultidimensionais.c
    Criando vetores com duas posições.

--- vetorMultidimensionais.c
+++ vetorMultidimensionais.c
@@ -7,18 +7,21 @@
 /* Início do corpo principal do programa */
 int main() {
   int matriz[LINHA][COLUNA], i, j;
-    for (i = 0; i < 3; i++) {
-      for(j=0 ; j < 3; j++){
+    for (i = 1; i < 3; i++) {
+      for(j=1 ; j < 3; j++){
         printf("\nEntre com o elemento [%d][%d]",i,j);
         scanf("%d",&matriz[i][j]);
       }
     }
 
-    for (i = 0; i < 3; i++) {
-      for(j=0 ; j < 3; j++){
+    for (i = 1; i < 3; i++) {
+      for(j=1 ; j < 3; j++){
         if(i==j){
           printf("\n%d", matriz[i][j]);
+/* Ao final do processamento serão exibidos todos os elementos que façam parte da diagonal principal.
+          */
         }
       }  
-    } 
+    }
+  
 }
