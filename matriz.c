154d3fe8864c2be50b1f4e88a31ad2a71532ec01
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2023-11-16 22:37:23 -0300

    vetorMedia.c
    Calculo de média de 3 notas com vetor

--- matriz.c
+++ matriz.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-double vetorNotas[3];
-double media;
+float vetorNotas[3];
+float media;
+int i;
 
 /* Início do corpo principal do programa */
-int main(void) {
-  for (int i = 0; i < 3; i++) {
-    printf("Digite a nota %d: ", i + 1);
-    scanf("%lf", &vetorNotas[i]);
+int main() {
+  for (i = 0; i < 3; i++) {
+    printf("Digite a %dº nota : \n", i + 1);
+    scanf("%f", &vetorNotas[i]);
+    media += vetorNotas[i];
   }
+  media = media / 3;
+  printf("A média é: %.2f \n", media);
+
 }
