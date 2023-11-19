4d88c38a9b5df04c05d6ead7b72aefd78f9ad772
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2023-11-19 16:47:41 -0300

    Create primeiroCodigo.c

---
+++ primeiroCodigo.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Escrita e leitura
+#include <stdlib.h> // Biblioteca auxiliar.
+
+// Aula 01 - Esqueleto de um programa em C
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
