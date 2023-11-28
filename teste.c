ba24351a204b71e66410dad145789f476b054c64
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2023-11-27 22:54:52 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -25,15 +25,6 @@ if((*pRaiz) != NULL) {
  exibirPreOrdem(&(*pRaiz)->direita); }
 }
 
-void exibirEmOrdem(No **pRaiz) {
-if((*pRaiz) != NULL) {
- exibirEmOrdem(&(*pRaiz)->esquerda);
- printf(“%i\n”, (*pRaiz)->numero);
- exibirEmOrdem(&(*pRaiz)->direita); }
-}
 
-void exibirPosOrdem(No **pRaiz) {
-if((*pRaiz) != NULL) {
- exibirPosOrdem(&(*pRaiz)->esquerda);
  exibirPosOrdem(&(*pRaiz)->direita);
  printf(“%i\n”, (*pRaiz)->numero); }
