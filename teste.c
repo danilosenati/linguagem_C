819e7da9adc09dac775d9eb1354260745271234b
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2023-12-05 23:25:45 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,28 +1,5 @@
-void exibirPreOrdem(No **pRaiz) {
-if((*pRaiz) != NULL) {
- printf(“%i\n”, (*pRaiz)->numero);
- exibirPreOrdem(&(*pRaiz)->esquerda);
- exibirPreOrdem(&(*pRaiz)->direita); }
-}
-
-void exibirEmOrdem(No **pRaiz) {
-if((*pRaiz) != NULL) {
- exibirEmOrdem(&(*pRaiz)->esquerda);
- printf(“%i\n”, (*pRaiz)->numero);
- exibirEmOrdem(&(*pRaiz)->direita); }
-}
-
-void exibirPosOrdem(No **pRaiz) {
-if((*pRaiz) != NULL) {
- exibirPosOrdem(&(*pRaiz)->esquerda);
- exibirPosOrdem(&(*pRaiz)->direita);
- printf(“%i\n”, (*pRaiz)->numero); }
-
-void exibirPreOrdem(No **pRaiz) {
-if((*pRaiz) != NULL) {
- printf(“%i\n”, (*pRaiz)->numero);
- exibirPreOrdem(&(*pRaiz)->esquerda);
- exibirPreOrdem(&(*pRaiz)->direita); }
+int main(){
+ Oi!!
 }
 
 
