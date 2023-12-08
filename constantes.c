c5001185f607c39ac5dcebcb61902f8cf5a200c3
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2023-12-08 14:19:23 -0300

    Update constantes.c

--- constantes.c
+++ constantes.c
@@ -5,13 +5,18 @@
 // Aula 06 - Constantes
 
 int main(void) {
-  // outra forma de declarar constante
+// Outra forma de declarar constante
   const float PI2 = 3.1415;
-  
-  // Declaração de variáveis
+
+// Declaração de variáveis
   float valor = (PI + PI2) + 1;
-  
+
+// Forma incorreta de declarar constante:
+  PI =  3.1415;
+
+// Imprimindo Saida de dados:
   printf("Valor do 1º PI: %f \n",PI);
    printf("Valor do 2º PI: %f \n",PI);
   printf("Soma de %f + %f  + 1 =  %f", PI, PI2, valor);
+  return 0;
 }
